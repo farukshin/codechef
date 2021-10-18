a22034976b6bf083fe1a0930e9e1d77fd3ea71bf
Amir Farukshin <farukshin.amir@gmail.com>
2021-10-18 21:34:44 +0300

    snckql21

---
+++ SNCKQL21/LUCKYNUM.cpp
@@ -0,0 +1,18 @@
+// https://www.codechef.com/SNCKQL21/problems/LUCKYNUM
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+void solve()
+{
+	int a,b,c;
+	cin>>a>>b>>c;
+	cout<<(a==7||b==7||c==7?"YES":"NO")<<"\n";
+}
+
+int main()
+{
+	int t;cin>>t;while(t--)
+	solve();
+    return 0;
+}
